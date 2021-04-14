babdfaa7226700a8f16b5c8328f87e86420cb744
eylay <ali.seyfi68@gmail.com>
2021-04-14 14:32:41 +0430

    05 Change Fonts and Styles

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -32,9 +32,11 @@
                 </label>
             </div>
 
-            <div class="flex items-center justify-end mt-4">
+            <hr class="my-4">
+
+            <div class="flex items-center justify-between">
                 @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
+                    <a class="text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
                         {{ __('Forgot your password?') }}
                     </a>
                 @endif
