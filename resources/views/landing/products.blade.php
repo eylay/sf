2306509787e40b8c18d0caa08645582386835563
eylay <ali.seyfi68@gmail.com>
2021-04-21 18:42:54 +0430

    Public Product Cards

--- resources/views/landing/products.blade.php
+++ resources/views/landing/products.blade.php
@@ -2,6 +2,23 @@
 @section('content')
 
     <h4> محصولات </h4>
-    <hr>
+    <div class="row">
+        @foreach ($products as $product)
+            <div class="col-md-4 my-3 product-card">
+                <div class="d-flex justify-content-between">
+                    <h5> {{$product->title}} </h5>
+                    <p>
+                        @if ($product->discount)
+                            <span class="text-danger off mx-2"> {{number_format($product->price)}} </span>
+                        @endif
+                        <span> {{number_format($product->cost)}} </span>
+                    </p>
+                </div>
+                <hr>
+                <img src="{{asset($product->image)}}">
+                <p> {{$product->description}} </p>
+            </div>
+        @endforeach
+    </div>
 
 @endsection
