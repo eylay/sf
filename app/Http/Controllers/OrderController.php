ce2e4924d978a731384798ac525eed5462165179
eylay <ali.seyfi68@gmail.com>
2021-05-18 08:33:23 +0430

    List Orders in All Panels - Part 1

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Cart as Order;
 use App\Models\CartItem;
+use App\Models\Product;
+use App\Models\Shop;
 
 class OrderController extends Controller
 {
@@ -17,8 +19,27 @@ class OrderController extends Controller
 
     public function index()
     {
-        $orders = Order::paginate(10);
-        return view('order.index', compact('orders'));
+        $currentLoggedInUser = auth()->user();
+
+        if ($currentLoggedInUser->role == 'shop') {
+
+            $currentShop = Shop::where('user_id', $currentLoggedInUser->id)->first();
+            $pids = Product::where('shop_id', $currentShop->id)->pluck('id')->toArray();
+            $items = CartItem::whereIn('product_id', $pids)->paginate(10);
+            return view('order.shop_index', compact('items'));
+
+        }else {
+
+            $orders = Order::query();
+
+            if ($currentLoggedInUser->role == 'user') {
+                $orders = $orders->where('user_id', $currentLoggedInUser->id);
+            }
+
+            $orders = $orders->paginate(10);
+            return view('order.index', compact('orders'));
+
+        }
     }
 
     public function show(Order $order)
